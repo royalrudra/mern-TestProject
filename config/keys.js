49accf3b5e3f891b8238306ee08b5acfca5e4dc9
rudra singh <royal4rudra@gmail.com>
2018-11-13 11:40:16 +0545

    Initial express server with test routes files

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://rudra:rudra123@ds161183.mlab.com:61183/mernconnector"
+};
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+const app = express();
+
+//DB config
+const db = require("./config/keys").mongoURI;
+
+//Connect to Mongo Db
+mongoose
+  .connect(db)
+  .then(() => console.log("Mongo db connected"))
+  .catch(err => console.log("error", err));
+
+app.get("/", (req, res) => {
+  res.send("Hello there");
+});
+
+//Use Routes
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`server running on port ${port}`);
+});
